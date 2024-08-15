02983493da0d88b16ddc989d5c5d2390f7014f4c
Guest14 <changheon3619@gmail.com>
2024-08-15 18:36:00 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { WebSocketServer } = require("ws");
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8000, () => {
+    console.log('8000');
+})
+
+
+const wss = new WebSocketServer({ port : 8001 });
+
+wss.on("connection", (ws,request) => {
+    wss.clients.forEach(client => {
+        client.send('new user'+wss.clients.size);
+    })
+
+    console.log('new user'+request.socket.remoteAddress);
+
+    ws.on("close", () => {
+        wss.clients.forEach((client) => {
+            client.send('user out'+wss.clients.size);
+        })
+    })
+
+    ws.on("message", data => {
+        wss.clients.forEach(client => {
+            client.send(data.toString());
+        })
+    })
+})
